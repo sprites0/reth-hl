209a806c835e0a4ca5de2f97c02af783eadd5c82
sprites0 <199826320+sprites0@users.noreply.github.com>
2025-07-19 21:21:59 +0000

    chore: lint

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod call_forwarder;
 pub mod chainspec;
 pub mod consensus;
 mod evm;
@@ -6,6 +7,5 @@ pub mod hl_node_compliance;
 pub mod node;
 pub mod pseudo_peer;
 pub mod tx_forwarder;
-pub mod call_forwarder;
 
 pub use node::primitives::{HlBlock, HlBlockBody, HlPrimitives};
